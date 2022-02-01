3d3cc83eb864118714b046a64e054ebe3485b95a
Ashmita Srivastava <asrivastava@cogitate.us>
2022-02-01 15:41:30 +0530

    chat-app first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const http = require('http')
+const port = process.env.PORT || 3004
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+const path = require('path')
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+var name = "";
+
+io.on('connection', (socket) => {
+
+    console.log("new user arrived")
+
+    socket.on('joining msg', (username) =>
+    {
+        name = username
+        //send information to other sockets
+        io.emit('chat message', name+" has joined the chat")
+    })
+
+    socket.on('chat message', (msg) => {
+        //will send the message to all socket expect the sending socket
+        socket.broadcast.emit('chat message', msg)
+    })
+
+})
+
+server.listen(port, () => {
+    console.log("server started at ", port)
+})
